f70f21ef64573458ad2502428b1c73d3b3a855ec
An Bast <sebastianrondonja@hotmail.com>
2022-12-30 01:08:43 -0500

    Redireccion a otra ruta

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,13 +11,23 @@ Los tipos de validaciones dependen del tipo de guard generado dentro de la clase
 */
 export class AuthGuard implements CanActivate {
 
+  constructor(private router: Router) { }
+
   /*
   Maneja la logica para validar si se puede o no ingresar a la ruta asociada a este guardian
+  */
+
+  /*
+  Podemos redireccionar a otra pagina desde un .ts de la siguiente forma:
+  this.router.navigate(['home']);
+  Esto requiere la importacion necesaria y la inyeccion de dependencia corespondiente
   */
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    //por ejemplo en caso de que sea falsa nuestra validacion podemos retornar otra ruta diferente
+    //this.router.navigate(['recovery']);
     return true;
   }
 
